acc193a2e0145468893831c73383257c3c5303a0
Juan Roussilian <juancruzroussilian9@gmail.com>
2024-09-03 17:58:50 -0300

    Agrego timeout a comando netcat en validar echo server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+    #!/bin/bash
 
 # Para verificar el funcionamiento, levantar cliente con:
 # docker compose -f docker-compose-dev.yaml up -d client1
 # ya que es el cliente quien se encarga de comprobar el 
 # funcionamiento del servidor
 
-SERVER_RESPONSE=$(docker exec -i client1 sh -c 'echo -n "test" | nc 172.25.125.2 12345')
+SERVER_RESPONSE=$(docker exec -i client1 sh -c 'echo -n "test" | nc -w 5 172.25.125.2 12345')
 if [ "$SERVER_RESPONSE" == "test" ]; then
     echo "action: test_echo_server | result: success"
 else
