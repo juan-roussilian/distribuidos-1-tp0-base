c5a5bc90df69ec9aeb8b94018814fcb20dfe23db
Juan Roussilian <juancruzroussilian9@gmail.com>
2024-09-04 03:09:33 -0300

    Utilizo operadores disponibles en sh en lugar de bash

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,7 +7,7 @@
 docker compose -f docker-compose-dev.yaml up -d server-checker
 SERVER_RESPONSE=$(docker exec server-checker sh -c "echo -n 'test' | nc -w 1 server 12345")
 docker compose -f docker-compose-dev.yaml stop server-checker
-if [ "$SERVER_RESPONSE" == "test" ]; then
+if [ "$SERVER_RESPONSE" = "test" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
