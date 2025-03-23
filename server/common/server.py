c751dc74f9e56cc0895349f6636ac293297417b4
Juan Roussilian <juancruzroussilian9@gmail.com>
2025-03-23 01:39:30 -0300

    corrijo log que debia estar dentro de condicion en manejador de sigterm de server

--- server/common/server.py
+++ server/common/server.py
@@ -30,7 +30,7 @@ class Server:
             logging.info(f'action: close | result: success | resource type: server socket')
             if self.active_connection is not None:
                 self.active_connection.close()
-            logging.info(f'action: close | result: success | resource type: client socket | ip: {self.active_connection_addr[0]}')
+                logging.info(f'action: close | result: success | resource type: client socket | ip: {self.active_connection_addr[0]}')
             quit()
         return sigterm_handler
     
