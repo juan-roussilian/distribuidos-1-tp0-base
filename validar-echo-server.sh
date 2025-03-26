53c324b2ed0578d977e397dfc2d33a9bd0fad81c
Juan Roussilian <juancruzroussilian9@gmail.com>
2025-03-26 02:32:16 -0300

    elimino contenedor server checker y realizo prueba de servidor con docker run

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-docker compose -f docker-compose-dev.yaml up -d server-checker
-SERVER_RESPONSE=$(docker exec server-checker sh -c "echo -n 'test' | nc -w 1 server 12345")
-docker compose -f docker-compose-dev.yaml stop server-checker
+SERVER_RESPONSE=$(docker run --rm -i --network tp0_testing_net --entrypoint sh gophernet/netcat -c 'echo "test" | nc server 12345')
 if [ "$SERVER_RESPONSE" = "test" ]; then
     echo "action: test_echo_server | result: success"
 else
