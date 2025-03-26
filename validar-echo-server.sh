e8d36434aac6b13e0b69a98e993a113975245882
Juan Roussilian <juancruzroussilian9@gmail.com>
2025-03-26 12:52:16 -0300

    reemplazo gophernet por busybox

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SERVER_RESPONSE=$(docker run --rm -i --network tp0_testing_net --entrypoint sh gophernet/netcat -c 'echo "test" | nc server 12345')
+SERVER_RESPONSE=$(docker run --rm -i --network tp0_testing_net --entrypoint sh busybox -c 'echo "test" | nc server 12345')
 if [ "$SERVER_RESPONSE" = "test" ]; then
     echo "action: test_echo_server | result: success"
 else
